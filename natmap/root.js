648e315a64c8a7b03459f09522b63d96785e1deb
Mats Henrikson <mats.henrikson@data61.csiro.au>
2020-12-10 17:50:25 +1100

    Let's get started with the Agriculture group.

--- natmap/root.js
+++ natmap/root.js
@@ -2,8 +2,40 @@
 const getFromCatalogPath = require('../helpers/getFromCatalogPath');
 const natmap20200903v8 = require('./in/natmap-2020-09-03-v8.json');
 
-// Grab just National Datasets
-const catalog = [getFromCatalogPath(natmap20200903v8, ["National Datasets"])];
-
-module.exports = Object.assign({}, natmap20200903v8, { catalog: catalog });
-
+module.exports = {
+  catalog: [
+    {
+      name: "National Datasets",
+      type: "group",
+      members: [
+        {
+          name: "Agriculture",
+          type: "group",
+          members: [
+            ...getFromCatalogPath(natmap20200903v8, ["National Datasets","Agriculture"])
+              .members.filter(m => m.name !== "Land Use and Cover in South Australia")
+          ]
+        }
+      ]
+    }
+  ],
+  workbench: [],
+  corsDomains: [
+    "corsproxy.com",
+    "programs.communications.gov.au",
+    "mapsengine.google.com",
+    "s3-ap-southeast-2.amazonaws.com",
+    "data.melbourne.vic.gov.au",
+    "data.act.gov.au",
+    "dea.ga.gov.au",
+    "gsky.nci.org.au",
+    "discover.data.vic.gov.au",
+    "data.gov.au"
+  ],
+  homeCamera: {
+    north: -8,
+    east: 158,
+    south: -45,
+    west: 109
+  }
+}

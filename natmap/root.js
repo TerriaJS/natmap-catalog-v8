cd684c1cced2c5d454c5191372207b8824acaab9
Mats Henrikson <mats.henrikson@data61.csiro.au>
2020-12-11 16:30:30 +1100

    Removing NIDEM from Elevation - Elevation needs more work.

--- natmap/root.js
+++ natmap/root.js
@@ -3,9 +3,11 @@ const _ = require('lodash');
 const getFromCatalogPath = require('../helpers/getFromCatalogPath');
 const natmap20200903v8 = require('./in/natmap-2020-09-03-v8.json');
 
+
 // remove "Land Use" subgroup from Agriculture
 const Agriculture = _.cloneDeep(getFromCatalogPath(natmap20200903v8, ["National Datasets", "Agriculture"]));
 Agriculture.members = Agriculture.members.filter(m => m.name !== "Land Use and Cover in South Australia");
+
 
 // remove ABC Photo Stories from Communications
 const Communications = _.cloneDeep(getFromCatalogPath(natmap20200903v8, ["National Datasets", "Communications"]));
@@ -13,11 +15,22 @@ Communications.members = Communications.members
                             .filter(m => m.name !== "ABC Photo Stories (2009-2014)")
                             .filter(m => m.name !== "ABC Photo Stories by date");
 
+
+const Elevation = _.cloneDeep(getFromCatalogPath(natmap20200903v8, ["National Datasets", "Elevation"]));
+// TODO: can we keep the shareKey of the deleted NIDEM and add it to another occurrence of the layer?
+// remove Intertidial/NIDEM layer
+const nidemId = getFromCatalogPath(natmap20200903v8, ["National Datasets", "Elevation",
+                  "Intertidal", "Intertidal elevation model", "NIDEM - Intertidal elevation model"])
+                  .id;
+Elevation.members = Elevation.members.filter(m => m.name !== "Intertidal");
+
+
 // assemble the National Datasets group
 const NationalDatasets = _.cloneDeep(getFromCatalogPath(natmap20200903v8, ["National Datasets"]));
 NationalDatasets.members = [
     Agriculture,
-    Communications
+    Communications,
+    Elevation
 ]
 
 // put the National Datasets into the catalog
